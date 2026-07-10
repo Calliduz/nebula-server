769bde1e07df96c5bafe70e73dbd4c03d7b5c02d
calliduz <juztyned@gmail.com>
2026-07-10 12:01:38 +0800

    Added nextgencloudfabric origin and referer to vaplayer headers

--- utils/vaplayer.ts
+++ utils/vaplayer.ts
@@ -20,6 +20,8 @@ export class VaplayerScraper {
     const headers = {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+      "Origin": "https://nextgencloudfabric.com",
+      "Referer": "https://nextgencloudfabric.com/",
     };
 
     const gotOptions: any = {
